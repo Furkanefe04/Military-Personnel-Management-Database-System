2c96de60740a40b7ccd1d0e0cc4936d8210daf69
Furkanefe04 <Cinarfurkanefe@gmail.com>
2026-05-20 19:57:24 +0300

    final commit

--- convert_log.py
+++
@@ -1,7 +0,0 @@
-with open("sql_error.log", "r", encoding="utf-16-le") as f:
-    text = f.read()
-
-with open("sql_error_utf8.log", "w", encoding="utf-8") as f:
-    f.write(text)
-
-print("Done converting log.")
